a22f342309587d1c6738649015da6415906684b4
espemm <espemm@gmail.com>
2026-04-07 17:37:05 +0200

    Corregit docstring en provaf.py

--- src/provaf.py
+++ src/provaf.py
@@ -2,6 +2,11 @@
 Prova Final d'automatització
 """
 def sumar2(a,b):
+    '''
+    Sumar 2 valos enters.
+    param a: valor enter 1
+    param b: valor enter 2
+    '''
     return a+b
 
 # Programa Principal
